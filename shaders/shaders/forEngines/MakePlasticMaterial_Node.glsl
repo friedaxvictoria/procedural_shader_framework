cedcf3a635877e97a13d0103ecc9da00784ee2c5
saeedelsayed <saidelsayed922@gmail.com>
2025-06-21 20:51:16 +0200

    modularizing the dolphin with SDFs

---
+++ shaders/shaders/forEngines/MakePlasticMaterial_Node.glsl
@@ -0,0 +1,17 @@
+void MakePlasticMaterial(
+    vec3 Color,
+    vec3 specularcolor,
+    float specularstrength,
+    float shininess,
+    // Base Parameters
+    out vec3 BaseColor,
+    out vec3 SpecularColor,
+    out float SpecularStrength,
+    out float Shininess
+){
+    BaseColor = Color;
+    SpecularColor = SpecularColor;
+    SpecularStrength = SpecularStrength;
+    Shininess = shininess;
+    
+}
